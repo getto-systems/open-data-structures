d2f5b61ea4f1c6e79b476f90e25c52f09a5a681f
shun <shun@getto.systems>
2019-04-06 08:12:21 +0000

    refactor: test

--- test/chapter2/array_queue_test.rb
+++ test/chapter2/array_queue_test.rb
@@ -5,63 +5,83 @@ require "chapter2/array_queue"
 module OpenDataStructures::Chapter2
   class ArrayQueueTest < Minitest::Test
     def test_set_and_get
-      array_queue = ArrayQueue.new
-      array_queue.add(0,nil)
-      array_queue.add(0,nil)
+      array = ArrayQueue.new
+      array.add(0,nil)
+      array.add(0,nil)
 
-      array_queue[0] = :a
+      array[0] = :a
 
-      assert_equal :a, array_queue[0]
+      assert_equal :a, array[0]
 
-      array_queue[1] = :b
+      array[1] = :b
 
-      assert_equal :a, array_queue[0]
-      assert_equal :b, array_queue[1]
+      assert_equal :a, array[0]
+      assert_equal :b, array[1]
     end
 
     def test_add_and_remove
-      array_queue = ArrayQueue.new
+      array = ArrayQueue.new
 
-      array_queue.add(0,:a)
-      assert_equal :a, array_queue[0]
+      array.add(0,:a)
+      assert_equal :a, array[0]
 
-      array_queue.add(0,:b)
-      assert_equal :b, array_queue[0]
-      assert_equal :a, array_queue[1]
+      array.add(0,:b)
+      assert_equal :b, array[0]
+      assert_equal :a, array[1]
 
-      array_queue.add(0,:c)
-      assert_equal :c, array_queue[0]
-      assert_equal :b, array_queue[1]
-      assert_equal :a, array_queue[2]
+      array.add(0,:c)
+      assert_equal :c, array[0]
+      assert_equal :b, array[1]
+      assert_equal :a, array[2]
 
-      assert_equal :c, array_queue.remove(0)
-      assert_equal :b, array_queue[0]
-      assert_equal :a, array_queue[1]
+      assert_equal :c, array.remove(0)
+      assert_equal :b, array[0]
+      assert_equal :a, array[1]
 
-      assert_equal :b, array_queue.remove(0)
-      assert_equal :a, array_queue[0]
+      assert_equal :b, array.remove(0)
+      assert_equal :a, array[0]
 
-      assert_equal :a, array_queue.remove(0)
+      assert_equal :a, array.remove(0)
     end
 
     def test_resize
-      array_stack = ArrayQueue.new
+      array = ArrayDeque.new
 
-      array_stack.add(0,:a)
-      array_stack.add(0,:b)
-      array_stack.add(0,:c)
-      array_stack.add(0,:d)
-      array_stack.add(0,:e)
-      array_stack.add(0,:f)
+      array.add(0,:a)
+      array.add(0,:b)
+      array.add(0,:c)
+      array.add(0,:d)
+      array.add(0,:e)
+      array.add(0,:f)
+      array.add(0,:g)
+      array.add(0,:h)
+      array.add(0,:i)
+      array.add(0,:j)
+      array.add(0,:k)
+      array.add(0,:l)
+      array.add(0,:m)
+      array.add(0,:n)
+      array.add(0,:o)
+      array.add(0,:p)
 
-      assert_equal [:f,:e,:d,:c,:b,:a],
+      assert_equal [:p,:o,:n,:m,:l,:k,:j,:i,:h,:g,:f,:e,:d,:c,:b,:a],
         [
-          array_stack.remove(0),
-          array_stack.remove(0),
-          array_stack.remove(0),
-          array_stack.remove(0),
-          array_stack.remove(0),
-          array_stack.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
         ]
     end
   end
--- test/chapter2/rootish_array_stack_test.rb
+++ test/chapter2/rootish_array_stack_test.rb
@@ -5,63 +5,83 @@ require "chapter2/rootish_array_stack"
 module OpenDataStructures::Chapter2
   class RootishArrayStackTest < Minitest::Test
     def test_set_and_get
-      rootish_array_stack = RootishArrayStack.new
-      rootish_array_stack.add(0,nil)
-      rootish_array_stack.add(0,nil)
+      array = RootishArrayStack.new
+      array.add(0,nil)
+      array.add(0,nil)
 
-      rootish_array_stack[0] = :a
+      array[0] = :a
 
-      assert_equal :a, rootish_array_stack[0]
+      assert_equal :a, array[0]
 
-      rootish_array_stack[1] = :b
+      array[1] = :b
 
-      assert_equal :a, rootish_array_stack[0]
-      assert_equal :b, rootish_array_stack[1]
+      assert_equal :a, array[0]
+      assert_equal :b, array[1]
     end
 
     def test_add_and_remove
-      rootish_array_stack = RootishArrayStack.new
+      array = RootishArrayStack.new
 
-      rootish_array_stack.add(0,:a)
-      assert_equal :a, rootish_array_stack[0]
+      array.add(0,:a)
+      assert_equal :a, array[0]
 
-      rootish_array_stack.add(0,:b)
-      assert_equal :b, rootish_array_stack[0]
-      assert_equal :a, rootish_array_stack[1]
+      array.add(0,:b)
+      assert_equal :b, array[0]
+      assert_equal :a, array[1]
 
-      rootish_array_stack.add(0,:c)
-      assert_equal :c, rootish_array_stack[0]
-      assert_equal :b, rootish_array_stack[1]
-      assert_equal :a, rootish_array_stack[2]
+      array.add(0,:c)
+      assert_equal :c, array[0]
+      assert_equal :b, array[1]
+      assert_equal :a, array[2]
 
-      assert_equal :c, rootish_array_stack.remove(0)
-      assert_equal :b, rootish_array_stack[0]
-      assert_equal :a, rootish_array_stack[1]
+      assert_equal :c, array.remove(0)
+      assert_equal :b, array[0]
+      assert_equal :a, array[1]
 
-      assert_equal :b, rootish_array_stack.remove(0)
-      assert_equal :a, rootish_array_stack[0]
+      assert_equal :b, array.remove(0)
+      assert_equal :a, array[0]
 
-      assert_equal :a, rootish_array_stack.remove(0)
+      assert_equal :a, array.remove(0)
     end
 
     def test_resize
-      rootish_array_stack = RootishArrayStack.new
+      array = ArrayDeque.new
 
-      rootish_array_stack.add(0,:a)
-      rootish_array_stack.add(0,:b)
-      rootish_array_stack.add(0,:c)
-      rootish_array_stack.add(0,:d)
-      rootish_array_stack.add(0,:e)
-      rootish_array_stack.add(0,:f)
+      array.add(0,:a)
+      array.add(0,:b)
+      array.add(0,:c)
+      array.add(0,:d)
+      array.add(0,:e)
+      array.add(0,:f)
+      array.add(0,:g)
+      array.add(0,:h)
+      array.add(0,:i)
+      array.add(0,:j)
+      array.add(0,:k)
+      array.add(0,:l)
+      array.add(0,:m)
+      array.add(0,:n)
+      array.add(0,:o)
+      array.add(0,:p)
 
-      assert_equal [:f,:e,:d,:c,:b,:a],
+      assert_equal [:p,:o,:n,:m,:l,:k,:j,:i,:h,:g,:f,:e,:d,:c,:b,:a],
         [
-          rootish_array_stack.remove(0),
-          rootish_array_stack.remove(0),
-          rootish_array_stack.remove(0),
-          rootish_array_stack.remove(0),
-          rootish_array_stack.remove(0),
-          rootish_array_stack.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
+          array.remove(0),
         ]
     end
   end
